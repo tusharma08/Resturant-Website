773cd10caf282c330feb50f942ab8ca9faf9e5df
Tushar Sharma <tushar.sh08@gmail.com>
2024-09-21 13:42:37 +0530

    modified:   backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,3 @@ export const  connectDB = async () =>{
     await mongoose.connect('mongodb+srv://tushar:tushar8826@cluster0.xt95pkr.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
